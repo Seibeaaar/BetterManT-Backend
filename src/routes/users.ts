d877d0ab26be307cf87ce625cc013aa18e4b1460
Yurii Baryliak <yuriibaryliak@Yuriis-MacBook-Pro.local>
2023-07-08 21:07:37 +0300

    Use decoded user id to create a new user document

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -6,12 +6,13 @@ const users = Router();
 
 users.post("/create", verifyToken, validateUserCreate, async (req, res) => {
   try {
+    const userId = res.locals as unknown as string;
     const userDoc = {
       ...req.body,
       surveyCompleted: false,
       createdAt: Date.now(),
     };
-    const user = await Database.create("users", userDoc);
+    const user = await Database.create("users", userDoc, userId);
     res.status(201).send(user);
   } catch (e) {
     res.status(500).send("Something went wrong");
